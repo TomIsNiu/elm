ba700aff632b7006bf048f020fab674a3ebf80a2
lanou\lanou <1060617679>
2019-10-20 16:47:22 +0800

    定

--- vueELM/src/components/bDing.vue
+++ vueELM/src/components/bDing.vue
@@ -10,7 +10,17 @@
         <div class="header">
           <img :src=lastInfo[2] alt="">
           <span>{{lastInfo[1]}}</span>
-          <button class="but">支付</button>
+          <button class="but" @click="zhi">支付</button>
+          <transition name="bounce">
+            <div class="btan" v-if="p">
+              <div class="btan1">
+                <span class="btan2"></span>
+                <span  class="btan3"></span>
+              </div>
+              <p class="btan4">暂不支持支付功能</p>
+              <div class="btan5"  @click="yin">确认</div>
+            </div>
+          </transition>
         </div>
         <li class="food-item" v-for="(v,i) in cartInfo" :key="i">
           <p class="food-name">{{v[0]}}</p>
@@ -30,11 +40,24 @@
           return {
             lastInfo:[],
             cartInfo:[],
+            p:false
           }
       },
       created(){
         this.lastInfo=JSON.parse(localStorage.getItem("history4"));
         this.cartInfo=JSON.parse(localStorage.getItem("history5"));
+      },
+      methods:{
+       zhi(){
+          if (this.p==false){
+            this.p=true;
+          }
+        },
+        yin(){
+          if (this.p==true){
+            this.p=false;
+          }
+        },
       }
     }
 </script>
@@ -114,4 +137,89 @@
     border: 0;
     border-radius: 0.2rem;
   }
+
+  .btan{
+    background-color:#4cd964;
+    width: 90%;
+    margin: 1rem auto;
+  }
+  .btan{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-top: -6rem;
+    margin-left: -6rem;
+    width: 12rem;
+    animation: tipMove .4s;
+    background-color: #fff;
+    padding-top: .6rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    border: 1px;
+    border-radius: .25rem;
+  }
+  .bounce-enter-active {
+    animation: bounce-in .5s;
+  }
+  .bounce-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+  .btan1{
+    width: 3rem;
+    height: 3rem;
+    border: .15rem solid #f8cb86;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+  .btan2{
+    width: .12rem;
+    height: 1.5rem;
+    background-color: #f8cb86;
+  }
+  .btan3{
+    width: .2rem;
+    height: .2rem;
+    border: 1px;
+    border-radius: 50%;
+    margin-top: .2rem;
+    background-color: #f8cb86;
+  }
+  /*.btan4{*/
+  /*font-size: 1.5rem;*/
+  /*color: #333;*/
+  /*line-height: .9rem;*/
+  /*text-align: center;*/
+  /*margin-top: .8rem;*/
+  /*padding: 0 .4rem;*/
+  /*}*/
+  .btan5{
+    font-size: .8rem;
+    color: #fff;
+    font-weight: 700;
+    /*margin-top: .8rem;*/
+    background-color: #4cd964;
+    width: 100%;
+    text-align: center;
+    line-height: 1.8rem;
+    border: 1px;
+    border-bottom-left-radius: .25rem;
+    border-bottom-right-radius: .25rem;
+  }
+
 </style>
